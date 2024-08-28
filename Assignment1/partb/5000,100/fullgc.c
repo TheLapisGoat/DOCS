787519a4594ce084f92e1115e6afaad3218796c0
thelapisgoat <sourodeepdatta@gmail.com>
2024-08-28 16:04:14 +0530

    Added Assignment 1

---
+++ Assignment1/partb/5000,100/fullgc.c
@@ -0,0 +1,28 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int main() {
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    
+    // Stopping GC
+    lua_gc(L, LUA_GCSTOP, 0);
+
+    // Running the testbench
+    if (luaL_dofile(L, "./testbench.lua")) {
+        fprintf(stderr, "Error: %s\n", lua_tostring(L, -1));
+        return 1;
+    }
+
+    lua_getglobal(L, "test");
+    lua_pcall(L, 0, 0, 0);
+
+    // Calling collect
+    lua_gc(L, LUA_GCCOLLECT, 0);
+
+    // Closing the Lua state
+    lua_close(L);
+
+    return 0;
+}
---
+++ Assignment1/partb/default/incrementalgc.c
@@ -0,0 +1,25 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int main() {
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    
+    // Setting GC to incremental mode
+    lua_gc(L, LUA_GCINC, 0);
+
+    // Running the testbench
+    if (luaL_dofile(L, "./testbench.lua")) {
+        fprintf(stderr, "Error: %s\n", lua_tostring(L, -1));
+        return 1;
+    }
+
+    lua_getglobal(L, "test");
+    lua_pcall(L, 0, 0, 0);
+
+    // Closing the Lua state
+    lua_close(L);
+
+    return 0;
+}
